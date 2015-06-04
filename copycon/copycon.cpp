592e9ff1e059bfa2f69872ce8f41ce725cec985b
Mark Redekopp <redekopp@usc.edu>
2015-06-04 14:13:04 -0700

    Added STL and copycon

---
+++ copycon/copycon.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class MyArray
+{
+ public:
+  MyArray() { dat = NULL; len = 0; }
+  MyArray(int d[], int num); //normal
+  ~MyArray();
+  int get(int loc) const     { return dat[loc]; }
+  void set(int loc, int val) { dat[loc] = val;  }
+  int getLen() const         { return len;      }
+ private:
+  int len; int *dat;
+
+};
+// Normal constructor
+MyArray::MyArray(int d[], int num)
+{
+  dat = new int[num]; len = num;
+  for(int i=0; i < len; i++){
+    dat[i] = d[i];
+  }
+}
+
+
+MyArray::~MyArray()
+{
+  cout << "In destructor" << endl;
+  delete [] dat;
+}
+void printVals(MyArray arr)
+{
+  for(int i=0; i < arr.getLen(); i++){
+    cout << arr.get(i) << " ";
+  }
+  cout << endl;
+}
+
+int main()
+{
+  int vals[] = {9,3,7,5};
+  MyArray a1(vals,4);
+  MyArray a2(a1); // calls default copy
+  MyArray a3 = a1; // calls default copy
+  MyArray a4; 
+  a4 = a1; // calls default assignment
+  // how are the contents of a2, a3, a4
+  // related to a1
+
+  return 0;
+}
